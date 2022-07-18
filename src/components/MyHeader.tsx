55e5504a5b1339c1dbfbd54351e902fc89ddf704
EngJao89 <eng_joao@outlook.com>
2022-07-17 22:55:41 -0300

    Closes 03:"Implementa primeiro header."

---
+++ src/components/MyHeader.tsx
@@ -0,0 +1,22 @@
+import { IonHeader, IonTitle, IonImg, IonToolbar, IonButtons, IonButton } from "@ionic/react";
+import React from "react";
+import './MyHeader.css';
+
+const MyHeader: React.FC = () => {
+  return (
+    <IonHeader class="ion-no-border">
+      <IonToolbar class="toolbarStyle">
+        <IonButtons slot="start">
+          <IonButton>About</IonButton>
+        </IonButtons>
+        <IonButtons slot="end">
+          <IonButton>Contact</IonButton>
+        </IonButtons>
+        <IonImg src="assets/images/logo.jpg" class="logoImg" />
+        <IonTitle class="title">Law Choice</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+  )
+}
+
+export default MyHeader;
