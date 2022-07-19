f869f724fe350068455e8695dc696c24df2cc726
EngJao89 <eng_joao@outlook.com>
2022-07-18 22:52:22 -0300

    Closes 03:"Implementa contacts."

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import About from './pages/About';
+import Contact from './pages/Contact';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
