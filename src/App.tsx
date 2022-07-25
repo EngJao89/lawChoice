10e66bfdf61375bc8b849bcbf52f3954910f51f4
EngJao89 <eng_joao@outlook.com>
2022-07-24 22:16:48 -0300

    Closes 09:"Implementa FAQ."

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import LawyerList from './pages/LawyerList';
 import LawyerDetail from './pages/LawyerDetail';
-//import Faq from './pages/Faq';
-//import FaqDetail from './pages/FaqDetail';
+import Faq from './pages/Faq';
+import FaqDetail from './pages/FaqDetail';
 //import Blog from './pages/Blog';
 //import BlogDetail from './pages/BlogDetail';
 
@@ -40,9 +40,9 @@ const App: React.FC = () => (
         {/*
         <Route path="/blogpost/:id" component={BlogDetail} exact={true} />
         <Route path="/blog" component={Blog} exact={true} />
+        */}
         <Route path="/faqdetail/:id" component={FaqDetail} exact={true} />
         <Route path="/faq" component={Faq} exact={true} />
-        */}
         <Route path="/lawyerdetail/:id" component={LawyerDetail} exact={true} />
         <Route path="/lawyerlist" component={LawyerList} exact={true} />
         <Route path="/contact" component={Contact} exact={true} />
