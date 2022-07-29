cad1a974f02c2f49faecc0b54a9a4228cc074305
EngJao89 <eng_joao@outlook.com>
2022-07-29 00:03:50 -0300

    Closes 17:"Melhora FAQ."

--- src/pages/FaqDetail.tsx
+++ src/pages/FaqDetail.tsx
@@ -5,8 +5,10 @@ import {
   IonIcon, 
   IonCard, 
   IonCardHeader, 
-  IonCardTitle,
-  IonCardContent 
+  IonCardTitle, 
+  IonCardContent, 
+  IonGrid, 
+  IonCol 
 } from '@ionic/react';
 import React, {useState} from 'react';
 import MyHeader from '../components/MyHeader';
@@ -16,25 +18,31 @@ import {arrowBackOutline} from 'ionicons/icons';
 const FaqDetail: React.FC<any> = ({match}) => {
   const [faqs] = useState<Array<any>>(FaqListing);
   const selectedFaq = faqs.find((issue)=>{
-      return issue.id === match.params.id;
+    return issue.id === match.params.id;
   });
 
   return(
     <IonPage>
-      <MyHeader />
       <IonContent>
-        <IonButton color="light" routerLink="/faq">
-          <IonIcon slot="start" icon={arrowBackOutline} />
-            Back
-        </IonButton>
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>{selectedFaq.title}</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            {selectedFaq.answer}
-          </IonCardContent>
-        </IonCard>
+        <IonGrid fixed={true}>
+          <IonCol size="3">
+            <IonButton color="light" routerLink="/faq">
+              <IonIcon slot="start" icon={arrowBackOutline} />
+                Back
+            </IonButton>
+          </IonCol>
+          <IonCol size="9">
+              <MyHeader />
+          </IonCol>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardTitle>{selectedFaq.title}</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              {selectedFaq.answer}
+            </IonCardContent>
+          </IonCard>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
